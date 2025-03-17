3e7b680f42def1fca08040ba665619fb1697bcfe
wchistow <wchistow@yandex.ru>
2025-03-17 13:00:56 +0300

    старт

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import tkinter
+
+VERSION = '1.0.0a0'
+
+
+def main():
+    tk = tkinter.Tk()
+    tk.title(f'pyKuMir v{VERSION}')
+
+    tkinter.mainloop()
+
+if __name__ == "__main__":
+    main()
