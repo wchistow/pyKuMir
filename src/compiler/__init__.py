8ce10b41df8365f41a0d1d0b6df23feedd0d0360
wchistow <wchistow@yandex.ru>
2025-05-14 14:35:33 +0300

    `compiler.__init__.py`: `code_to_bytecode` -> `code2bc`

--- src/compiler/__init__.py
+++ src/compiler/__init__.py
@@ -1,10 +1,13 @@
 from .build_bytecode import build_bytecode
 from .parser import Parser, improve
 
+from .bytecode import BytecodeType
 from .exceptions import SyntaxException, RuntimeException
 from .vm import VM
 
+__all__ = ['build_bytecode', 'Parser', 'improve', 'SyntaxException', 'RuntimeException', 'VM']
 
-def code_to_bytecode(code: str) -> list[tuple]:
-    p = Parser()
-    return build_bytecode(improve(p.parse(code)))
+
+def code2bc(code: str) -> tuple[list[BytecodeType], dict[str, list[BytecodeType]]]:
+    p = Parser(code)
+    return build_bytecode(improve(p.parse()))
