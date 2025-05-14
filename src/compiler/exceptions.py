7dd9d320c48d38a3dc692d6c1c426bf6ce64dc27
wchistow <wchistow@yandex.ru>
2025-05-14 14:34:54 +0300

    переписал исключения

--- src/compiler/exceptions.py
+++ src/compiler/exceptions.py
@@ -1,9 +1,16 @@
-class SyntaxException(SyntaxError):
+class KumirException(Exception): ...
+
+
+class SyntaxException(KumirException):
     def __init__(self, line: int, token=None, message: str | None = None):
         if message is None:
             message = f'здесь не должно быть "{token}"'
-        self.args = (line, token, message)
+        self.line = line
+        self.token = token
+        self.args = (message + f' (строка {line})',)
 
-class RuntimeException(SyntaxError):
+
+class RuntimeException(KumirException):
     def __init__(self, line: int, message: str):
-        self.args = (line, message)
+        self.line = line
+        self.args = (message + f' (строка {line})',)
