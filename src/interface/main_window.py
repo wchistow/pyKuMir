6d6fe4c2564bbbd5e9f1958785f2d28683264063
wchistow <wchistow@yandex.ru>
2025-06-06 15:55:00 +0300

    небольшие исправления интерфейса

--- src/interface/main_window.py
+++ src/interface/main_window.py
@@ -2,8 +2,8 @@ from threading import Thread
 from locale import getencoding
 from platform import python_version, python_implementation, platform
 
-from PyQt6.QtWidgets import (QWidget, QMenuBar, QMenu, QSplitter, QHBoxLayout,
-                             QGridLayout, QPushButton, QMessageBox)
+from PyQt6.QtWidgets import (QWidget, QMenuBar, QMenu, QSplitter, QGridLayout,
+                             QMessageBox, QToolBar, QToolButton)
 from PyQt6.QtCore import Qt, QT_VERSION_STR, PYQT_VERSION_STR
 
 from .codeinput import CodeInput
@@ -44,10 +44,8 @@ class MainWindow(QWidget):
         self.help_menu.addAction('О программе', self.show_about)
         self.menu_bar.addMenu(self.help_menu)
 
-        self.buttons = QHBoxLayout()
-        self.run_button = QPushButton('запустить')
-        self.run_button.clicked.connect(self.run_code)
-        self.buttons.addWidget(self.run_button)
+        self.buttons = QToolBar()
+        self.add_tool_buttons()
 
         self.codeinput = CodeInput(self)
         self.console = Console(self)
@@ -60,13 +58,19 @@ class MainWindow(QWidget):
         self.runner_thread: Thread | None = None
 
         self.grid = QGridLayout(self)
-        self.grid.addWidget(self.menu_bar, 0, 0, 0, 0)
-        self.grid.addLayout(self.buttons, 1, 0, 1, 1)
-        self.grid.addWidget(self.code_and_console, 2, 0, 7, 1)
+        self.grid.setMenuBar(self.menu_bar)
+        self.grid.addWidget(self.buttons, 0, 0, 1, 1)
+        self.grid.addWidget(self.code_and_console, 1, 0, 7, 1)
 
         self.setMinimumSize(600, 500)
 
         self.setLayout(self.grid)
+
+    def add_tool_buttons(self):
+        run_button = QToolButton()
+        run_button.setText('запустить')
+        run_button.clicked.connect(self.run_code)
+        self.buttons.addWidget(run_button)
 
     def closeEvent(self, a0):
         if self.runner_thread is not None:
